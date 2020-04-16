a80fcc3b9e03392b8dacf48cbe576eeb4e98ef92
HughParsonage <hugh.parsonage@gmail.com>
2020-04-16 17:19:30 +1000

    Add description of simulate.R

--- R/simulate.R
+++ R/simulate.R
@@ -2,7 +2,41 @@
 
 
 simulate_sa2 <- function() {
-  NULL
+  ## Each day a person can
+  ## stay in the household
+  ## journey outside
+  ## be admitted to hospital etsq
+
+  ## if a person journeys out they do so for a purpose
+  ## they can move to a different SA2
+  ## or they can move for a daily activity
+
+  ## if they go out for work
+  ## their destination is their dzn of work
+  ## unless they are work in a hospital, school, or aged care
+
+  ## if they go out for school as a pupil
+  ## (only occurs if they are a child)
+  ## they go to a school in their SA2
+
+  ## if they go out to purchase groceries
+  ## the destination is stochastic
+  ## Other places by predefined times
+  ## per year
+
+  ## Only interested in magnitude of interactions
+  ## (above a certain limit?)
+
+  ## Process is
+  ## Loop over each day
+  ### Loop over each person
+  #### identify their places that day
+  ### add up all the interactions
+  ### sleep!
+  ### next day
+
+
+
 }
 
 
