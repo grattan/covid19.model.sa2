fc740a4574ba8381e7c8c3e0c8c570643b27dff6
HughParsonage <hugh.parsonage@gmail.com>
2020-04-20 03:48:09 +1000

    Document options

---
+++ R/covid19.model.sa2-opts.R
@@ -0,0 +1,13 @@
+#' covid19.model.sa2 package options
+#'
+#' @section Options:
+#'
+#' The following package options are available and set to the
+#' given default when loaded.
+#' \describe{
+#' \item{\code{covid19.model.sa2_dataEnv = new.env()}}{Environment into which data will be saved/accessed.}
+#' \item{\code{covid19.model.sa2_nThread = 1L}}{Number of threads to use.}
+#' }
+#'
+#'
+NULL
