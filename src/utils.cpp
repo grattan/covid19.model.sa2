5804427c218b62e1ba99a7dd2d1633bd131ae5b1
HughParsonage <hugh.parsonage@gmail.com>
2020-04-22 03:12:10 +1000

    Add max0

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,3 +5,11 @@ using namespace Rcpp;
 int maxii(int a, int b) {
   return (a < b) ? b : a;
 }
+
+int max0(int x) {
+  return (x > 0) ? x : 0;
+}
+
+int max0(double x) {
+  return (x > 0) ? x : 0;
+}
