80e74a1029f458e848933d1ff190267ba426d872
HughParsonage <hugh.parsonage@gmail.com>
2020-04-23 17:59:21 +1000

    Fixes to set_initial
    * Correct logic so works in simulate2
    * Provides deterministic distributions rather than (often wild) random ones
      (i.e. if you say dead = 4 you get dead = 4, rather than approximately 4.)

--- tests/testthat/test-set_initial.R
+++ tests/testthat/test-set_initial.R
@@ -1,16 +1,19 @@
 test_that("set_initial_by_state works", {
+  skip_if_not_installed("data.table")
+  library(data.table)
+  aus <- read_sys("australia.fst")
+  aus[, status := set_initial_by_state(state)]
+  expect_true(is.integer(aus[["status"]]))  # essentially check that the defaults are ok
+
+
   A <- set_initial_by_state(1L, .population = 10e3)
-  expect_equal(sum(A), 10e3)
-  B <-
-  withr::with_seed(55L, {
-    set_initial_by_state(1L,
-                         dead = 10L,
-                         healed = 100L,
-                         active = 400L,
-                         critical = 10L,
-                         .population = 10e3)
-  })
-  expect_true(sum(B == -2) == 12L)
-  expect_true(sum(B == -1) == 111L)
+  expect_equal(length(A), 10e3)
+
+  B <- set_initial_by_state(1L,
+                            dead = 10, healed = 100, active = 400, critical = 50,
+                            .population = 500e3)
+  expect_equal(sum(B == status_killed()), 10)
+  expect_equal(sum(B == status_critic()), 50)
+
 
 })
