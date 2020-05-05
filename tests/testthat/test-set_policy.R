5d33b383672b761efd7f01def75094e0c6b2d99a
HughParsonage <hugh.parsonage@gmail.com>
2020-05-05 17:48:21 +1000

    Fix wrong test

--- tests/testthat/test-set_policy.R
+++ tests/testthat/test-set_policy.R
@@ -1,6 +1,6 @@
 test_that("set_policy works with states", {
   for (d in 1:5) {
     out <- set_policypars(school_days_per_wk = list(NSW = d))
-    expect_equal(out$NSW[1], d)
+    expect_equal(dollars(out, school_days_per_wk, NSW)[1], d)
   }
 })
