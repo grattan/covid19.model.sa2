fcb8637ff454a439d06fdeb455e3d373894499e8
HughParsonage <hugh.parsonage@gmail.com>
2020-05-22 03:16:48 +1000

    Move update_pappubahry as user-visible
    Since #45 is possible, this is more reasonable to expose

---
+++ R/update_pappubahry.R
@@ -0,0 +1,38 @@
+#' Update the time series data from pappubahry/COVID19
+#'
+#' @return
+#' A named logical vector, indicating success for the given file.
+#'
+#' Invoked for its side-effect: changing the internal \code{fst} files
+#' giving the time series of COVID-19 cases from
+#' \url{https://github.com/HughParsonage/pappubahry-AU_COVID19}.
+#'
+#' Note that this changes the package's files: any original files will be
+#' overwritten.
+#'
+#' @export
+
+update_pappubahry <- function() {
+
+  # When n > 3 we functionalize
+  read_pappu <- function(nom) {
+    stopifnot(!endsWith(nom, ".csv"))
+    github_url <- paste0("https://github.com/pappubahry/AU_COVID19/raw/master/", nom, ".csv")
+    dataraw.csv <- paste0("data-raw/pappubahry/AU_COVID19/", nom, ".csv")
+    extdata.fst <- system.file("extdata", paste0(nom, "fst"), package = packageName())
+    fread(github_url) %T>%
+      fwrite(provide.file(dataraw.csv)) %>%
+      .[, Date := as.Date(Date)] %>%
+      setkey(Date) %>%
+      write_fst(extdata.fst, compress = 100)
+
+    TRUE
+  }
+
+  sapply(c("time_series_cases", "time_series_deaths",
+           "time_series_recovered", "time_series_tests"),
+         read_pappu)
+
+}
+
+
