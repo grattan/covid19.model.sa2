ff8b0a1bac8103e0d2f87a31f1ea45403806003d
HughParsonage <hugh.parsonage@gmail.com>
2020-06-08 23:18:06 +1000

    Add tests, esp contact tracing

--- tests/testthat/test-set_initial.R
+++ tests/testthat/test-set_initial.R
@@ -38,7 +38,37 @@ test_that("set_initial retrospectively", {
 
   # Number of cases should be consistent with cumulative cases
   expect_true(abs(S$Statuses[V1 != 0, .N] - 4860L) < 50L)
+})
 
+
+test_that("Set initial by state using a wide tribble", {
+  skip_on_cran()
+  skip_if(is32bit())
+  skip_if_not_installed("withr")
+  skip_if_not_installed("tibble")
+  skip_if_not_installed("data.table")
+
+  manual_initial_status <-
+    tibble::tribble(
+      ~state, ~active, ~critical, ~dead, ~healed,
+      "NSW",     100,         9,     6,      30,
+      "VIC",      40,         5,     4,      20,
+      "QLD",      30,         2,     3,      10,
+       "SA",      10,         0,     0,      10,
+       "WA",      12,         0,     0,      10,
+      "TAS",      20,         0,     0,       1,
+       "NT",       1,         0,     0,       3,
+      "ACT",    1000,         0,     0,       1,
+      "OTH",     100,         0,     0,       0)
+
+  library(data.table)
+  S <- simulate_sa2(5,
+                    .first_day = "2020-04-01",
+                    InitialStatus = manual_initial_status,
+                    EpiPars = set_epipars(incubation_distribution = "dirac", incubation_mean = 100L),
+                    PolicyPars = set_policy_no_restrictions())
+  n_act_active <- S$Statuses[and3s(state == 8, Status %in% 1:3), .N]
+  expect_true(n_act_active %between% c(900, 1100))
 
 })
 
